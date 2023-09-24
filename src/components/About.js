f8e6052f283ffefa0d3b082018c2a40e697311c5
yago-pixel <yagolira02@hotmail.com>
2023-09-24 19:43:02 -0400

    updating About.js

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
+import avatar from "../images/Portfolio_Headshot.png";
+import "../style/About.css";
+
+function About() {
+  return (
+    // About section
+    <section id="about" className="py-5">
+      <h1 className="about-heading">About Me</h1>
+      <Container>
+        <Row>
+          <Col
+            xs={12}
+            md={4}
+            className="d-flex justify-content-center align-items-center mb-4 mb-md-0"
+          >
+            <Image src={avatar} rounded fluid className="about-image" />
+          </Col>
+          <Col xs={12} md={8}>
+            <Card></Card>
