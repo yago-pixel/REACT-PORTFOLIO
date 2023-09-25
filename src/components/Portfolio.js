f0a07eddcbbbb7de4e0859d9e1234d97f7611ebd
yago-pixel <yagolira02@hotmail.com>
2023-09-24 20:21:41 -0400

    updating Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,3 +7,22 @@ import StackDev from "../images/StackDev.JPEG";
 import ThingsToDoInToronto from "../images/10ThingsToDoInToronto.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../style/Portfolio.css";
+// Portfolio component
+function Portfolio() {
+    return (
+          // Portfolio container
+      <div className="portfolio-wrapper">
+        <h1 className="portfolio-title">Portfolio</h1>
+        <div className="container">
+          <div className="row">
+            {[
+              {
+                title: "Thomas' Chess Blog",
+                description: "CMS-Style Model View Controller Blog",
+                imgSrc: Yago Lira,
+                deployedLink:
+                "https://github.com/yago-pixel/REACT-PORTFOLIO.git"
+                githubLink:
+                  "git@github.com:yago-pixel/REACT-PORTFOLIO.git",
+              },
+              
