4f5d25d882df8faad979eb462f9b31c258fad3d5
yago-pixel <yagolira02@hotmail.com>
2023-09-24 20:07:04 -0400

    updating Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,3 +5,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChessKnight } from "@fortawesome/free-solid-svg-icons";
 import { Container, Row, Col } from "react-bootstrap";
 import "../style/Header.css";
+// Header component
+function Header() {
+    return (
+      // Header component
+      <>
+        <Container fluid className="bg-dark">
+          <Row className="justify-content-center text-center mb-0">
+            <Col className="pb-0">
+              <h1 className="text-white" style={{ fontSize: "70px" }}>
+                <FontAwesomeIcon
+                  icon={faChessKnight}
+                  className="fa-knight"
+                  size="xl"
+                  style={{ color: "#white" }}
+                />
+                <Link
+                  to="/7homas7he7ank-react-portfolio/"
+                  className="text-white text-decoration-none"
+                  style={{ margin: "0" }}
