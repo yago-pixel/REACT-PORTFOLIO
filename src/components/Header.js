66aa8fba6e40a454713c2b6a78346d420197fea2
yago-pixel <yagolira02@hotmail.com>
2023-09-24 20:06:18 -0400

    updating Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navigation from "./Navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChessKnight } from "@fortawesome/free-solid-svg-icons";
+import { Container, Row, Col } from "react-bootstrap";
+import "../style/Header.css";
