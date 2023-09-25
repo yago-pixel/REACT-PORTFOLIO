23d2c9e996fb6116553cc5fbfda0b449bc198dea
yago-pixel <yagolira02@hotmail.com>
2023-09-24 20:19:27 -0400

    Portfolio.js

---
+++ src/components/Portfolio.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Project from "./Project";
+import WorkDayPlanner from "../images/WorkDayPlanner.GIF";
+import WebAPI from "../images/WebAPI_.GIF";
+import Weather from "../images/Weather.GIF";
+import StackDev from "../images/StackDev.JPEG";
+import ThingsToDoInToronto from "../images/10ThingsToDoInToronto.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/Portfolio.css";
